8e5868eeb43d6f6d45b0ebf88b41e9e8b7346ba7
Kristof Bach <crys@crys.hu>
2020-06-21 20:11:19 +0200

    WebIRC tweaks

--- test/server.ts
+++ test/server.ts
@@ -50,7 +50,7 @@ describe("Server", function () {
 		it("should run a web server on " + webURL, async () => {
 			const response = await got(webURL);
 			expect(response.statusCode).to.equal(200);
-			expect(response.body).to.include("<title>The Lounge</title>");
+			expect(response.body).to.include("<title>GoDClan WebChat</title>");
 			expect(response.body).to.include('type="module"');
 		});
 
